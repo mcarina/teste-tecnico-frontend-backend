e0539d9716714747c6f85736bec491d2078d9fb1
mcarina <marciacarina931@gmail.com>
2025-04-08 09:16:15 -0400

    inciando tela de login

--- front-end/dev.Dockerfile
+++ front-end/dev.Dockerfile
@@ -4,7 +4,7 @@ WORKDIR /app
 
 COPY package*.json ./
 
-RUN npm install --force
+RUN npm install -g @angular/cli && npm install --force
 
 COPY . .
 
---
+++ front-end/src/app/pages/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    LoginLayoutComponent
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
